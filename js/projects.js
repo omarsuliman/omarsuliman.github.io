8b8243e5a094fd9dc532fd8cfbe6b2151abfb37d
Omar Suliman <omarsulyman@gmail.com>
2017-03-24 01:22:47 +0300

    add code test with js in project page

---
+++ js/projects.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	one();
+})
+
+function one(){
+	$('#aSearch').click(function(e){
+		e.preventDefault();
+		$.ajax({
+			url: 'https://api.github.com/users/omarsuliman/repos?callback',
+		    type: 'get',
+		    dataType: 'json',
+		    success: function(data){
+			$.each(data,function(i,n){
+				$('.result').append('<li><a href="'+data[i]+'">'+data[i].name+'</a></li>')
+			})
+		  }
+	    })
+	})
+};
+
+
