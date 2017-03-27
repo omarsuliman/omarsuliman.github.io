b3cab572c6e6d6a2f5a14ef7166fed39864d3ec1
Omar Suliman <omarsulyman@gmail.com>
2017-03-27 10:35:23 +0300

    Modify project page and add my Favorite repos

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
 	one();
 	two();
+	abc();
+	abcd();
 })
 
 function one(){
 	$('#aSearch').click(function(e){
+		$('.result').html('')
 		e.preventDefault();
 		$.ajax({
-			url: 'https://api.github.com/users/omarsuliman/repos?callback',
+			url: 'https://api.github.com/users/omarsuliman/repos?sort=created&per_page=5',
 		    type: 'get',
 		    dataType: 'json',
 		    success: function(data){
 			    $.each(data,function(i,n){
-				  $('.result').append('<li><a href="'+data[i].html_url+'">'+data[i].name+'</a></li>')
+			    	var d=new Date(data[i].created_at)
+				  $('.result').append('<li><a href="'+data[i].html_url+'">'+data[i].name+'<br>Created at: '+formatDate(d)+'</a></li>')
+				  // $('.result').append('<li><a href="'+data[i].html_url+'">'+data[i].created_at+'</a></li>')
 			    })
 		    }
 	    })
@@ -20,19 +25,41 @@ function one(){
 };
 
 function two() {
-	$('#bSearch').click(function(e){
-		e.preventDefault();
-		$.ajax({
-			url: 'https://api.github.com/users/omarsuliman/repos?callback',
-		    type: 'get',
-		    dataType: 'json',
-		    success: function(data){
-			    $.each(data,function(i,n){
-				  $('.result').append('<li><a href="'+data[i].html_url+'">'+data[i].name+'</a></li>')
-			    })
-		    }
-	    })
-	})
+	$( "#showr" ).click(function() {
+  $( "#pop" ).first().show( "fast", function showNext() {
+    $( this ).next( "#pop" ).show( "fast", showNext );
+  });
+});
+ 
+$( "#hidr" ).click(function() {
+  $( "#pop" ).hide( 1000 );
+});
 }
 
 
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
+
+function abc(){
+	$("#aSearch").click(function(){
+		$("#pop").hide(1000);
+		$(".result").show(1000);
+
+	})
+}
+
+function abcd(){
+	$("#showr").click(function(){
+		$(".result").hide(1000);
+		$("#pop").show(1000);
+
+	})
+}
--- js/skills.js
+++ js/skills.js
@@ -1,22 +1,11 @@
+
 $(document).ready(function() {
 
     $.getJSON('skills.json', function(jd) {
-       $("h2").append("<li>"+jd.omar[0]+"</li>");
-       $("h2").append("<li>"+jd.omar[1]+"</li>");
-       $("h2").append("<li>"+jd.omar[2]+"</li>");
-       $("h2").append("<li>"+jd.omar[3]+"</li>");
-       $("h2").append("<li>"+jd.omar[4]+"</li>");
-       $("h2").append("<li>"+jd.omar[5]+"</li>");
-    });	
+
+        $.each(jd.omar,function(i){
+
+		 $('h2').append("<li>"+jd.omar[i]+"</li>");
+		})
+    });		
 });
-
-// $(document).ready(function() {
-
-//     $.getJSON('skills.json', function(jd) {
-
-//         $.each(jd,function(i,n){
-
-// 		 $('h2').append("<li>"+jd.omar[0]+"</li>");
-// 		})
-//     });		
-// });
