428487e6398d3d52dcc7f5ff4aa05f0aa962c183
Omar Suliman <omarsulyman@gmail.com>
2017-04-02 03:47:52 +0300

    Modyfy some code in more.js page

--- js/more.js
+++ js/more.js
@@ -1,32 +1,43 @@
 $(document).ready(function(){
 
-	$('#one').click(function(){
-
+   $(".re").hide();
+	// $('#one').click(function(){
+        $(".re").show();
 		$.ajax({
 			url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId=PLnY5rTcrX-j5Lg7C6i7Lq1XOnVVAUuXhJ&key=AIzaSyDVNOTsxna2PaOwbA9VHmd814hHPpk08jk',
 		    type: 'get',
 		    dataType: 'json',
 		    success: function(data){
-		    	// var title = 'data.items[0].snippet.description'
-			   // $('.re').append('<li><a href="'+data.items[0].snippet.description+'">'+'</a></li>')
-			     // $('.re').append('<li><a href="'+data.items[0].snippet.description+'">''</a></li>')	
-			     var videoId = 	data.items[0].snippet.resourceId.videoId;	      	
-                 $( ".re" ).append( "<p>"+data.items[0].snippet.title+"</p>" );
-                 $('#video').attr('src','https://www.youtube.com/embed/'+videoId);
-						// $( ".re" ).append('<embed width="420" height="315"src="'+data.items[0].snippet.+'">')
-			   console.log(data)
+		    	
+				var html= '';
+				 	html+= '<ul>'
+				 	$.each(data.items,function(i, item){
+				 		html+= '<br><li><a href="https://www.youtube.com/watch?v='+item.snippet.resourceId.videoId+'&list='+item.snippet.playlistId+'"><img src="'+item.snippet.thumbnails.medium.url+'"</img><br><br></a><span>'+ item.snippet.title +'</span></li>';
+				 	});
+				 	$('.re').html(html);
 		    }
 		})
 
-	})
+	// })
+
 })
 
 
 
 
+// '<iframe class="x1" width="40%" height="200px" id='video'src="'+videoId+'"></iframe>'
 
 
 
+
+// function showPlaylist(json){
+//  	var html= '';
+//  	html+= '<ul>'
+//  	$.each(json.items,function(i, item){
+//  		html+= '<li><a href="https://www.youtube.com/watch?v='+item.snippet.resourceId.videoId+'&list='+item.snippet.playlistId+'"><img src="'+item.snippet.thumbnails.medium.url+'"</img></a><span>'+ item.snippet.title +'</span></li>';
+//  	});
+//  	$('.container #success').html(html);	
+//  }
 
 
 
