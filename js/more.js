3cf32f07117997a83ad0a8ee23beaf548b17dbbb
Omar Suliman <omarsulyman@gmail.com>
2017-04-10 12:37:38 +0300

    Modified the More page & index page

--- js/more.js
+++ js/more.js
@@ -5,14 +5,20 @@ $(document).ready(function(){
 	    type: 'get',
 	    dataType: 'json',
 	    success: function(data){
-	    	console.log(data.items[0])
-	    	var vid = data.items[0].snippet.resourceId.videoId
+	    	// console.log(data.items[0])
 
 			var html= ''
+			    html+='<div class="embed-responsive embed-responsive-4by3">'
 			 	html+= '<ul>'
 			 	$.each(data.items,function(i, item){
-			 	html+= '<br><a href="https://www.youtube.com/watch?v='+item.snippet.resourceId.videoId+'&list='+item.snippet.playlistId+'"><img src="'+item.snippet.thumbnails.medium.url+'"</img><br><br></a><span>'+ item.snippet.title +'</span>';
+			 		console.log(item)
+
+                html+='<div>'
+                html+='<br><iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/'+item.snippet.resourceId.videoId+'" frameborder="0" allowfullscreen></iframe>'
+                // html+='<h3>'+item.snippet.title+'</h3>'
+                html+='<i class="fa fa-plus-square">'+item.snippet.title+'</i>'
 			 	html+= '</ul>'
+			 	html+='</div>'
 			});
 			$('.col-sm-6').html(html);
 			// $('.col-sm-6').html(html);
@@ -23,6 +29,8 @@ $(document).ready(function(){
 
 
 
+     
+    
 
 
 
